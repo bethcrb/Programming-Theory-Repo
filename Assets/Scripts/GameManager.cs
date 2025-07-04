fe64d29cfb50136712094a19223f24da7a3eb776
Beth <bethcrb@gmail.com>
2025-07-04 10:45:31 -0700

    Use ScoreText and HealthText instead
    - Use ScoreText and HealthText instead of ScoreNewText and HealthLeftText

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,26 +3,28 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public TextMeshProUGUI healthLeftText;
-    public TextMeshProUGUI scoreNewText;
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        healthLeftText.text = "100";
-        scoreNewText.text = "0";
+        healthText.text = "Health: 100";
+        scoreText.text = "Score: 0";
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerController playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        healthText.text = "Health: ";
         if (playerController.health <= 0)
         {
             playerController.health = 0;
-            healthLeftText.color = new Color32(255, 0, 0, 255);
+            healthText.color = new Color32(255, 0, 0, 255);
         }
-        healthLeftText.text = playerController.health.ToString();
-        scoreNewText.text = playerController.score.ToString();
+        healthText.text += playerController.health.ToString();
+        scoreText.text = $"Score: {playerController.score.ToString()}";
     }
 }
