464c3964a79bc756562e91b8e5d3db29a35ed392
Beth <bethcrb@gmail.com>
2025-07-03 17:57:13 -0700

    Separate health text
    - Create new text element to show health left

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI healthLeftText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        healthText.text = "Health: 100";
+        healthLeftText.text = "100";
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = $"Health: {GameObject.Find("Player").GetComponent<PlayerController>().health}";   
+        healthLeftText.text = GameObject.Find("Player").GetComponent<PlayerController>().health.ToString();   
     }
 }
